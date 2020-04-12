b7bf1095146dde691f9d817cc6d96712c00daa63
Boglarka Barabas <taikatta@gmail.com>
2020-04-12 21:04:06 +0100

    Accounts app added

---
+++ ms4/accounts/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+
+
+def register(request):
+    if request.method == 'POST':
+
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if the two passwords match
+        if password == password2:
+
+            # Check if username exists
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is already registered.')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is already used! Please choose a different one.')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
+                    
+                    # Login after register
+                    auth.login(request, user)
+                    messages.success(request, 'Welcome!')
+                    return redirect('index')    
+        else:
+            messages.error(request, 'Passwords do not match!')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Using authenticate() to verify a set of credentials:
+        user = auth.authenticate(username=username, password=password)
+
+        # Check if the credentials are valid for a backend:
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Welcome!')
+            return redirect('index')
+        else:
+            messages.error(request, "Invalid credentials, please try it again!")
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
--- ms4/ms4/urls.py
+++ ms4/ms4/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('products/', include('products.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
