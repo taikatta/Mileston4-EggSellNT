dd6a615cfd7d4b0c7ae31b6a95a9b42450201507
Boglarka Barabas <taikatta@gmail.com>
2020-04-12 13:03:17 +0100

    Products urls and templates added

--- ms4/products/views.py
+++ ms4/products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
