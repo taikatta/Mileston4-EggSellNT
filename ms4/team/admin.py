663e1331c9c0a1a6951dc13f5f0b2b84cc968cb1
Boglarka Barabas <taikatta@gmail.com>
2020-04-14 10:44:24 +0100

    team app created

---
+++ ms4/team/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Team
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'title')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+    
+
+admin.site.register(Team, TeamAdmin)
