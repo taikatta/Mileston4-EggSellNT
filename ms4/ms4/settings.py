b692362b745fa409455c906c8b4923d9d773668f
Boglarka Barabas <taikatta@gmail.com>
2020-04-16 14:28:54 +0100

    STRIPE_PUBLISHABLE and STRIPE_SECRET added to settings.py

--- ms4/ms4/settings.py
+++ ms4/ms4/settings.py
@@ -134,6 +134,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
+
 # Messages
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
