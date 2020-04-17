301415e913c6c8c8582c8ac605df20ac8996cd63
Boglarka Barabas <taikatta@gmail.com>
2020-04-17 14:02:30 +0100

    Eggs photo folder created, photos now uploading there, other egg photos deleted

--- ms4/products/models.py
+++ ms4/products/models.py
@@ -5,8 +5,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    photo_main = models.ImageField(upload_to='photos')
-    photo_1 = models.ImageField(upload_to='photos', blank=True)
+    photo_main = models.ImageField(upload_to='photos/eggs')
+    photo_1 = models.ImageField(upload_to='photos/eggs', blank=True)
 
     def __str__(self):
         return self.name
