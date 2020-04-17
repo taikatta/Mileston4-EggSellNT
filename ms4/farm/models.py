397825bc6c734881144ecc591d8b0244fc193290
Boglarka Barabas <taikatta@gmail.com>
2020-04-17 21:40:54 +0100

    farm app added

---
+++ ms4/farm/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Farm(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/farm')
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
