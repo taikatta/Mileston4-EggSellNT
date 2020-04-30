9ade543b85a9fb71fc5ed9140344ea264c8bb9ba
Boglarka Barabas <taikatta@gmail.com>
2020-04-30 08:18:46 +0100

    Company app readded

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from company.models import Employee
 
 
 def index(request):
@@ -6,3 +7,11 @@ def index(request):
     Displaying the index page.
     """
     return render(request, "pages/index.html")
+
+
+def about(request):
+    """
+    Displaying the about us page.
+    """
+    employees = Employee.objects.all()
+    return render(request, "pages/about.html", {"employees": employees})
